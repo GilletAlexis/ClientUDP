fd8b2552ae0aec2ec39f23cd318f9c4b6c3c3c5a
GilletAlexis <gilletale@gmail.com>
2018-12-20 12:24:04 +0100

    Envoie du programme fini

--- ClientUDP.py
+++ ClientUDP.py
@@ -9,17 +9,24 @@ sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
 
 data, addr = sock.recvfrom(1024)
 print ("received message: ", data)
-
 sock.sendto(b"cinema", (UDP_IP, UDP_PORT))
-data, addr = sock.recvfrom(1024)
-print ("received message: ", data)
 
-octet1 = hex(15) and (data>>2)
-#octet2 = b'\xff' and (data >> 4)
-#octet3 = b'\xff' and (data >> 8)
-#octet4 = b'\xff' and (data >> 12)
-print (octet1)
+while True:
+    data, addr = sock.recvfrom(1024)
+    print ("received message: ", data)
 
-#sock.sendto(code, (UDP_IP, UDP_PORT))
-#data, addr = sock.recvfrom(1024)
-#print ("received message: ", data)
+    i=0
+    code = 0
+    taboctet = []
+    for i in range(4):
+        taboctet.append(data[i])
+        #data >> 8
+    code = data[3]<<24 | data[2]<<16 | data[1]<<8 | data[0]
+    
+    print(taboctet)
+    print(code)
+
+    code = str(code)
+    sock.sendto(code.encode(), (UDP_IP, UDP_PORT))
+    data, addr = sock.recvfrom(1024)
+    print ("received message: ", data)
